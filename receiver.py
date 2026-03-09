bfeae6fb236e41cab8ac6403238d5110ae3bad89
raywang999 <raywang999@gmail.com>
2026-03-09 01:59:05 -0400

    update argparse

--- receiver.py
+++ receiver.py
@@ -1,26 +1,20 @@
 import argparse
 
 
-def _port(value: str) -> int:
-    port = int(value)
-    if port < 1 or port > 65535:
-        raise argparse.ArgumentTypeError("port must be in range 1-65535")
-    return port
-
-
 def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         "emulator_host",
         help="hostname for the network emulator",
     )
     parser.add_argument(
         "emulator_ack_port",
-        type=_port,
+        type=int,
         help="UDP port number used by the link emulator to receive ACKs from the receiver",
     )
     parser.add_argument(
         "receiver_data_port",
-        type=_port,
+        type=int,
         help="UDP port number used by the receiver to receive data from the emulator",
     )
     parser.add_argument(
